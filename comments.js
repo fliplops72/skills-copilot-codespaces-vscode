764a8458c1b5874b32de7351837c63d067285fa7
fliplops72 <162298546+fliplops72@users.noreply.github.com>
2024-03-05 10:16:57 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,21 @@
+// Create web server
+// 1. Load the http module
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+// 2. Create a server
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+}).listen(8080);
+console.log('Server running at http://:8080/');
